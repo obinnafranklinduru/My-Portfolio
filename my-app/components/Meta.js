823552e96826cadc0f5e44ebd4a48de492774374
obinnafranklinduru <obinnafranklinduru.com>
2022-11-27 19:00:40 +0100

    initial commit

---
+++ my-app/components/Meta.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Meta = ({ title, keywords, description }) => {
+  return (
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='keywords' content={keywords} />
+      <meta name='description' content={description} />
+      <meta charSet='utf-8' />
+      <link rel='icon' href='/favicon.ico' />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Obinna Franklin Duru',
+  keywords: 'Blockchain Developer, Dapp Developer, NFTs Developer, Web Developer',
+  description: 'A top-notch Blockchain Developer, Smart Contract Developer, and Web Developer.',
+}
+
+export default Meta
